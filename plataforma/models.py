a0adc92268716361757395b1b94c6bff8b90ab6b
lucasmouradev <lucasmouraadev@gmail.com>
2022-07-09 03:52:14 -0300

    criado a pagina de pacientes com adição das imagens e caminhos nos botoẽs de cadastro e feita as validações necessárias do cadastro

---
+++ plataforma/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Pacientes(models.Model):
+    choices_sexo = (('F', 'Feminino'),
+                    ('M', 'Masculino'))
+    nome = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=1, choices=choices_sexo)
+    idade = models.IntegerField()
+    email = models.EmailField()
+    telefone = models.CharField(max_length=19)
+    nutri = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
