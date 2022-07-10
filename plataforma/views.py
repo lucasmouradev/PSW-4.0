9291999bfc85dedc41e07a96cea980ca8c885ee7
lucasmouradev <lucasmouraadev@gmail.com>
2022-07-10 02:42:52 -0300

    Adicionado pagina de cadastro dos clientes de cada nutricionista de acordo com o login, feita a inserção de dados e a criação de uma API para se comunicar com front para criação de um grafico baseado na evolução do peso do paciente

--- plataforma/views.py
+++ plataforma/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.messages import constants
-from .models import Pacientes
+from .models import Pacientes, DadosPaciente
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
 
 
 @login_required(login_url='/auth/logar/')
@@ -49,4 +51,73 @@ def pacientes(request):
                         return redirect('/pacientes/')
         except:
             messages.add_message(request, constants.ERROR, 'Erro interno do sistema')
-            return redirect('/pacientes/')            
+            return redirect('/pacientes/')
+
+
+
+
+@login_required(login_url='/auth/logar/')
+def dados_paciente_listar(request):
+    if request.method == "GET":
+        pacientes = Pacientes.objects.filter(nutri=request.user)
+        return render(request, 'dados_paciente_listar.html', {'pacientes': pacientes})    
+
+
+
+@login_required(login_url='/auth/logar/')
+def dados_paciente(request, id):
+    paciente = get_object_or_404(Pacientes, id=id)
+    if not paciente.nutri == request.user:
+        messages.add_message(request, constants.ERROR, 'Esse paciente não é seu')
+        return redirect('/dados_paciente/')
+    if request.method == "GET":
+        dados_paciente = DadosPaciente.objects.filter(paciente=paciente)
+
+        return render(request, 'dados_paciente.html', {'paciente': paciente, 'dados_paciente': dados_paciente})
+    
+    elif request.method == "POST":
+        peso = request.POST.get('peso')
+        altura = request.POST.get('altura')
+        gordura = request.POST.get('gordura')
+        musculo = request.POST.get('musculo')   
+
+        hdl = request.POST.get('hdl')
+        ldl = request.POST.get('ldl')
+        colesterol_total = request.POST.get('ctotal')
+        triglicerídios = request.POST.get('triglicerídios')
+
+
+        paciente = DadosPaciente(paciente=paciente,
+                                data=datetime.now(),
+                                peso=peso,
+                                altura=altura,
+                                percentual_gordura=gordura,
+                                percentual_musculo=musculo,
+                                colesterol_hdl=hdl,
+                                colesterol_ldl=ldl,
+                                colesterol_total=colesterol_total,
+                                trigliceridios=triglicerídios)
+
+        paciente.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Dados cadastrado com sucesso')
+
+
+        return redirect('/dados_paciente/')
+
+
+
+
+
+
+@login_required(login_url='/auth/logar/')
+@csrf_exempt
+def grafico_peso(request, id):
+    paciente = Pacientes.objects.get(id=id)
+    dados = DadosPaciente.objects.filter(paciente=paciente).order_by("data")
+    
+    pesos = [dado.peso for dado in dados]
+    labels = list(range(len(pesos)))
+    data = {'peso': pesos,
+            'labels': labels}
+    return JsonResponse(data)
